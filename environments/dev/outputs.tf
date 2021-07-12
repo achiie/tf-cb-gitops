6929a22cfa0b65308e58f74b40964e007862a0d7
Achyut <sharma.achyut17@gmail.com>
2021-07-12 16:11:23 +0530

    Updating gce-instances

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -12,9 +12,9 @@ output "firewall_rule" {
 }
 
 output "instance_name" {
-  value = "${module.http_server.instance_name}"
+  value = "${module.gce_instance.instance_name}"
 }
 
 output "external_ip" {
-  value = "${module.http_server.external_ip}"
+  value = "${module.gce_instance.external_ip}"
 }
