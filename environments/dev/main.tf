dfc2deea136b5b484a8336bedbe7d259b7afc9c6
Achyut <sharma.achyut17@gmail.com>
2021-07-12 15:53:48 +0530

    Updating dev-subnets

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,26 +1,26 @@
 
-locals {
-  env = "dev"
-}
+# locals {
+#   env = "dev"
+# }
 
-provider "google" {
-  project = "${var.project}"
-}
+# provider "google" {
+#   project = "${var.project}"
+# }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
+# module "vpc" {
+#   source  = "../../modules/vpc"
+#   project = "${var.project}"
+#   env     = "${local.env}"
+# }
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+# module "http_server" {
+#   source  = "../../modules/http_server"
+#   project = "${var.project}"
+#   subnet  = "${module.vpc.subnet}"
+# }
 
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+# module "firewall" {
+#   source  = "../../modules/firewall"
+#   project = "${var.project}"
+#   subnet  = "${module.vpc.subnet}"
+# }
