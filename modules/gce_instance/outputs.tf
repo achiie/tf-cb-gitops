a19bbc37f81762ea93481d811e2c1ba207a22dda
Achyut <sharma.achyut17@gmail.com>
2021-07-12 16:08:28 +0530

    Updating gce-instances

--- modules/gce_instance/outputs.tf
+++ modules/gce_instance/outputs.tf
@@ -1,9 +1,9 @@
 
 
 output "instance_name" {
-  value = "${google_compute_instance.http_server.name}"
+  value = "${google_compute_instance.gce_instance.name}"
 }
 
 output "external_ip" {
-  value = "${google_compute_instance.http_server.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance.gce_instance.network_interface.0.access_config.0.nat_ip}"
 }
